ab388908fb8e75b93579a0aaf7c9dfaed236cb98
JOSE LUIS PINAYA BALDERRAMA <jpinayab@doc.emi.edu.bo>
2024-11-19 02:41:28 -0400

    antes del literal

---
+++ Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,25 @@
+using Fantasy.Frontend.Repositories;
+using Fantasy.Shared.Entites;
+using Microsoft.AspNetCore.Components;
+
+namespace Fantasy.Frontend.Pages.Countries;
+
+public partial class CountriesIndex
+{
+    [Inject] private IRepository Repository { get; set; } = null!;
+    private List<Country>? Countries { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var responseHppt = await Repository.GetAsync<List<Country>>("api/countries");
+        Countries = responseHppt.Response;
+
+        //await LoadAsync();
+    }
+
+    //private async Task LoadAsync()
+    //{
+    //    var responseHppt = await Repository.GetAsync<List<Country>>("api/countries");
+    //    Countries = responseHppt.Response;
+    //}
+}
